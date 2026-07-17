16af3f09982d9139aa8b8243a38a0f6ffcc79e2c
Ernesto2018 <ernestondieki12@gmail.com>
2026-07-18 00:12:53 +0300

    Add zip reading capabilities

--- gui.py
+++ gui.py
@@ -4,13 +4,14 @@ Main application window for the WhatsApp chat viewer.
 
 from pathlib import Path
 
-from PyQt6.QtWidgets import QFileDialog, QMessageBox, QVBoxLayout, QWidget
 from PyQt6.QtGui import QIcon
+from PyQt6.QtWidgets import QFileDialog, QMessageBox, QVBoxLayout, QWidget
 
-from constants import APP_NAME, APP_ICON
-from customwidgets.dialogs import SenderNameDialog
+from constants import APP_ICON, APP_NAME
+from customwidgets.dialogs import EditDialog
 from customwidgets.groupboxes import ChatBox
 from models.messages import MessagesModel
+from utils import is_zip_encrypted
 
 
 class MainWindow(QWidget):
@@ -39,11 +40,12 @@ class MainWindow(QWidget):
         self.chat_viewer.file_btn.clicked.connect(self.open_chat_file)
 
         self.chat_viewer.sending.returnPressed.connect(self._reload_chat)
+        self.chat_viewer.zip_pwd.returnPressed.connect(self._reload_chat)
 
         mlayout.addWidget(self.chat_viewer)
 
     def open_chat_file(self, filename: str | None = None):
-        """open a file selection dialog and load the selected chat file"""
+        """use the selected filename to create messages model"""
 
         if not filename:
             # select from dialog
@@ -51,7 +53,7 @@ class MainWindow(QWidget):
                 self,
                 "Open WhatsApp Export",
                 str(self.last_known_dir),
-                "Text Files (*.txt)",
+                "Supported Files (*.txt *.zip)",
             )
 
         if not filename:
@@ -60,11 +62,19 @@ class MainWindow(QWidget):
         self.file_path = Path(filename)
         self.last_known_dir = self.file_path.parent
 
-        name_dialog = SenderNameDialog(self)
-        name_dialog.name_edit.setText(self.chat_viewer.sending.text().strip())
+        # check if zip and if encrypted
+        show_pwd = self.file_path.suffix == ".zip" and is_zip_encrypted(self.file_path)
+
+        # always toggle the password visibility
+        self.chat_viewer.toggle_pwd(show_pwd)
+
+        name_dialog = EditDialog(self)
+        name_dialog.set_text(self.chat_viewer.sendern())
+        name_dialog.toggle_password(show_pwd)
 
         if name_dialog.exec():
-            self.chat_viewer.sending.setText(name_dialog.name_edit.text())
+            self.chat_viewer.set_pwd(name_dialog.password())
+            self.chat_viewer.set_sendern(name_dialog.text())
 
         else:
             return
@@ -72,12 +82,12 @@ class MainWindow(QWidget):
         self._reload_chat()
 
     def _reload_chat(self):
-        """build the message index and display the chat"""
+        """build the message model and display the chat"""
 
         if self.file_path is None:
             return
 
-        sender = self.chat_viewer.sending.text().strip()
+        sender = self.chat_viewer.sendern()
 
         if not sender:
             QMessageBox.warning(
@@ -87,12 +97,15 @@ class MainWindow(QWidget):
             )
             return
 
+        # None or bytes
+        pwd = self.chat_viewer.pwd()
+
         # create a new messages model with the selected file and sender
-        model = MessagesModel(self.file_path, sender)
+        model = MessagesModel(self.file_path, sender, pwd)
         # set the model to the chat viewer
         self.chat_viewer.set_messages(model)
 
-    def closeEvent(self, event):
+    def closeEvent(self, a0):
         """close the messages model when the window is closed"""
         if model := self.chat_viewer.model():
             if isinstance(model, MessagesModel):
@@ -100,4 +113,4 @@ class MainWindow(QWidget):
                 # and won't freeze the UI
                 model.close()
 
-        super().closeEvent(event)
+        super().closeEvent(a0)
